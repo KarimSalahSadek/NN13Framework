615e5ffd76a7ec078da258675f46e6ad0d1df54c
Maher Magdy <61353846+Maher-Magdy@users.noreply.github.com>
2021-01-07 11:12:33 +0200

    Update activation_functions.py

--- nn13framework/neural_networks/activation_functions.py
+++ nn13framework/neural_networks/activation_functions.py
@@ -1,6 +1,6 @@
 #ALL FUNCTIONS TAKE NUMPY ARRAYS AS INPUT AND RETURN NUMPY ARRAYS!!!!
 #Numpy will automatically understand that 1x1 arrays are not arrays and return them correctly
-#All Funcitons are static methods that don't require an instance to call them
+#All Funcitons are static methods that don't require an instance to call 
 
 #If grad is set to True , return the derivative instead
 
