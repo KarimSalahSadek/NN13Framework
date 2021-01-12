3a1ad3dccdd2fbcf0f306dd978077339ab90d222
Maher Magdy <61353846+Maher-Magdy@users.noreply.github.com>
2021-01-13 01:52:10 +0200

    Update loss_functions.py

--- nn13framework/neural_networks/loss_functions.py
+++ nn13framework/neural_networks/loss_functions.py
@@ -6,24 +6,24 @@ import numpy as np
 import nn13framework.neural_networks.activation_functions as AF
 
 def mean_square_loss(prediction ,label,grad=False):
-    if label.shape==prediction.shape:
-        # the function output
-        col_len,row_len=label.shape
-        if grad == False:
-            return np.sum(((np.multiply((label-prediction),(label-prediction)))/(2*row_len)))
-        # the function derivative
-        elif grad == True:
-            return -((label-prediction)/row_len)
+    assert (label.shape == prediction.shape)
+    # the function output
+    col_len,row_len=label.shape
+    if grad == False:
+        return np.sum(((np.multiply((label-prediction),(label-prediction)))/(2*row_len)))
+    # the function derivative
+    elif grad == True:
+        return -((label-prediction)/row_len)
 
 
 
 def multinomial_loss(prediction ,label,grad=False):
-    if label.shape == prediction.shape:
-        # the function output
-        if grad == False:
-            return -np.sum((np.multiply(np.log(AF.softmax(prediction,False)),label )))
-        # the function derivative
-        elif grad == True:
-            # return the derivative for each class by d itself
-            return AF.softmax(prediction,False)-label
+    assert (label.shape == prediction.shape)
+    # the function output
+    if grad == False:
+        return -np.sum((np.multiply(np.log(AF.softmax(prediction,False)),label )))
+    # the function derivative
+    elif grad == True:
+        # return the derivative for each class by d itself
+        return AF.softmax(prediction,False)-label
 
