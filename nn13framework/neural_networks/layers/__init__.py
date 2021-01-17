Changed wieght initialization and fixed backpropagation

nn13framework/neural_networks/layers/__init__.py
@@ -22,6 +22,8 @@ class linear(layer):
             Data input must be a matrix of row vectors, each row vector is an example
         """
         data_in = np.array(data_in)
+        if self.layer_num != 0:
+            data_in = np.array(data_in).T
         if self.use_bias:
             data_out = np.dot(self.weight[:,:-1],data_in.T) + np.array([self.weight[:,-1]]).T #bias
         else:
@@ -34,9 +36,12 @@ class linear(layer):
         """
         Returns grad of weight , grad of input data
         """
-        grad_w = np.hstack((np.dot(grad_data_out.T,self.input),np.sum(grad_data_out.T,axis=-1,keepdims=True)))
-        #grad w = dw + db = sigma (grad_out_i * in_i) + sigma (grad_out_i)
-        grad_data_in = np.dot(self.weight[:,:-1].T,grad_data_out.T)
+        if self.use_bias:
+            grad_w = np.hstack((np.dot(grad_data_out,self.input),np.sum(grad_data_out,axis=1,keepdims=True)))
+            #grad w = dw + db = sigma (grad_out_i * in_i) + sigma (grad_out_i)
+        else:
+            grad_w = np.dot(grad_data_out,self.input)
+        grad_data_in = np.dot(self.weight[:,:-1].T,grad_data_out)
         return grad_w , grad_data_in
 
 ###########################################################################
@@ -60,8 +65,10 @@ class sigmoid(layer):
         """
         Returns None , grad of input data
         """
-        grad_data_in = activation_functions.sigmoid(self.last_input,grad=True)
-        return None , grad_data_in.T
+        grad_z = activation_functions.sigmoid(self.last_input,grad=True)
+        assert(grad_z.shape == data_out.shape)
+        grad_data_in = np.multiply(grad_z,data_out)
+        return None , grad_data_in
 
 class relu(layer):
     
@@ -82,8 +89,10 @@ class relu(layer):
         """
         Returns None , grad of input data
         """
-        grad_data_in = activation_functions.relu(self.last_input,grad=True)
-        return None , grad_data_in.T
+        grad_z = activation_functions.relu(self.last_input,grad=True)
+        assert(grad_z.shape == data_out.shape)
+        grad_data_in = np.multiply(grad_z,data_out)
+        return None , grad_data_in
 
 class softmax(layer):
     
@@ -97,6 +106,7 @@ class softmax(layer):
         
     def forward(self,data_in):
         self.last_input = data_in
+        
         data_out = activation_functions.softmax(data_in)
         return data_out
 
@@ -104,5 +114,7 @@ class softmax(layer):
         """
         Returns None , grad of input data
         """
-        grad_data_in = activation_functions.softmax(self.last_input,grad=True)
-        return None , grad_data_in.T
+        grad_z = activation_functions.softmax(self.last_input,grad=True)
+        assert(grad_z.shape == data_out.shape)
+        grad_data_in = np.multiply(grad_z,data_out)
+        return None , grad_data_in