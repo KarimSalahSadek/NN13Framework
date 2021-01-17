2fd2958e251652d013ce1dbe4d4af67d904c71b0
Maher Magdy <61353846+Maher-Magdy@users.noreply.github.com>
2021-01-17 16:43:19 +0200

    Update activation_functions.py

--- nn13framework/neural_networks/activation_functions.py
+++ nn13framework/neural_networks/activation_functions.py
@@ -27,7 +27,6 @@ def relu(z,grad=False):
 
 def softmax(z,grad=False):
     z=np.array(z)
-    z2=np.array(z)
     # the function output
     if grad == False:
         z=z-np.max(z,axis=1,keepdims=True)
@@ -35,30 +34,16 @@ def softmax(z,grad=False):
         return z/np.sum(z,axis=1,keepdims=True)
     # the function derivative
     elif grad == True:
-        # get number of examples = row_len , examle dimension = col_len
-        col_len, row_len = z.shape
-        # create a 3d matrix
-        grad_of_all_examples =np.zeros((row_len,col_len,col_len))
-        unity_matrix =np.identity(col_len)
-        for i in range(row_len):
-            #create x1 , x2 to hold the ith example
-            x1=np.zeros((col_len,col_len))
-            x2= np.zeros((col_len, col_len))
-            x3= np.zeros((col_len, col_len))
-            for j in range(col_len):
-                x1[:,[j]]=z[:, [i]]
-                #shift each x2 column
-                x2[:,[j]]=np.roll(z2[:, [i]], -j, axis = 0)
-            x3=np.array(x2)
-            # shift each x2 row in x3
-            for j in range(col_len):
-                for k in range(col_len):
-                    x3[j,k]=x2[j,k-j]
-            grad_of_all_examples[i]=(softmax(x1.T, False) * (unity_matrix - softmax(x3.T, False))).T
-        #return the final tensor
-        return np.sum(grad_of_all_examples,axis=0)
-        # return (softmax(z.T,False)*(1-softmax(z.T,False))).T
+        z=z.T
+        m, n = z.shape
+        p = softmax(z)
 
+        tensor1 = np.einsum('ij,ik->ijk', p, p)  # (m, n, n)
+
+        tensor2 = np.einsum('ij,jk->ijk', p, np.eye(n, n))  # (m, n, n)
+
+
+        return tensor2 - tensor1
 
 
 
