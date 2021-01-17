3d8cee340c5e330c7496d90f7a1930944270721e
Maher Magdy <61353846+Maher-Magdy@users.noreply.github.com>
2021-01-17 03:20:42 +0200

    softmax gradient

--- nn13framework/neural_networks/activation_functions.py
+++ nn13framework/neural_networks/activation_functions.py
@@ -2,6 +2,8 @@
 #Numpy will automatically understand that 1x1 arrays are not arrays and return them correctly
 #If grad is set to True , return the derivative instead
 import numpy as np
+from scipy.ndimage.interpolation import shift
+
 
 def sigmoid(x , grad = False):
     # the function output
@@ -25,6 +27,7 @@ def relu(z,grad=False):
 
 def softmax(z,grad=False):
     z=np.array(z)
+    z2=np.array(z)
     # the function output
     if grad == False:
         z=z-np.max(z,axis=1,keepdims=True)
@@ -32,8 +35,29 @@ def softmax(z,grad=False):
         return z/np.sum(z,axis=1,keepdims=True)
     # the function derivative
     elif grad == True:
-        # return the derivative for each class by d itself
-        return softmax(z,False)*(1-softmax(z,False))
+        # get number of examples = row_len , examle dimension = col_len
+        col_len, row_len = z.shape
+        # create a 3d matrix
+        grad_of_all_examples =np.zeros((row_len,col_len,col_len))
+        unity_matrix =np.identity(col_len)
+        for i in range(row_len):
+            #create x1 , x2 to hold the ith example
+            x1=np.zeros((col_len,col_len))
+            x2= np.zeros((col_len, col_len))
+            x3= np.zeros((col_len, col_len))
+            for j in range(col_len):
+                x1[:,[j]]=z[:, [i]]
+                #shift each x2 column
+                x2[:,[j]]=np.roll(z2[:, [i]], -j, axis = 0)
+            x3=np.array(x2)
+            # shift each x2 row in x3
+            for j in range(col_len):
+                for k in range(col_len):
+                    x3[j,k]=x2[j,k-j]
+            grad_of_all_examples[i]=(softmax(x1.T, False) * (unity_matrix - softmax(x3.T, False))).T
+        #return the final tensor
+        return np.sum(grad_of_all_examples,axis=0)
+        # return (softmax(z.T,False)*(1-softmax(z.T,False))).T
 
 
 
