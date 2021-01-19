Skipped the derivitive of softmax to solve a bug

nn13framework/neural_networks/layers/__init__.py
@@ -114,7 +114,13 @@ class softmax(layer):
         """
         Returns None , grad of input data
         """
-        grad_z = activation_functions.softmax(self.last_input,grad=True)
-        assert(grad_z.shape == data_out.shape)
-        grad_data_in = np.multiply(grad_z,data_out)
+        #grad_z = activation_functions.softmax(self.last_input,grad=True)
+        #assert(grad_z.shape == data_out.shape)
+        #grad_data_in = np.einsum('ijk,ik->ij', grad_z , data_out.T).T
+        grad_data_in = []
+        d_out = data_out.T
+        inp = self.last_input.T
+        for i in range(d_out.shape[0]):
+            grad_data_in.append(np.multiply(d_out,inp))
+        grad_data_in = np.sum(grad_data_in,axis=0).T
         return None , grad_data_in