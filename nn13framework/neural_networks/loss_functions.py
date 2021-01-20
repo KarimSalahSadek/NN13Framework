16c0ff1805856e4bb8010157f926266f076d3840
Maher Magdy <61353846+Maher-Magdy@users.noreply.github.com>
2021-01-20 19:42:27 +0200

    added the loss function class

--- nn13framework/neural_networks/loss_functions.py
+++ nn13framework/neural_networks/loss_functions.py
@@ -5,39 +5,63 @@
 import numpy as np
 import nn13framework.neural_networks.activation_functions as AF
 
-def mean_square_loss(prediction ,label,grad=False):
-    assert (label.shape == prediction.shape)
-    # transpose the matrices
-    label=np.transpose(label)
-    prediction=np.transpose(prediction)
-    # the function output
-    col_len,row_len=label.shape
-    if grad == False:
-        return np.sum(((np.multiply((label-prediction),(label-prediction)))/(2*row_len)))
-    # the function derivative
-    elif grad == True:
-        return -np.transpose(((label-prediction)/row_len))
+#parent class
+class loss_functions:
+    pass
+
+class mean_square_loss(loss_functions):
+    # general attributes
+    loss_derivative = None
+    #init functions that take the model
+    def __init__(self,model=None):
+        self.model=model
+        self.loss_derivative = None
+    #calculate the function and the derivative but only return the function
+    def evaluate(self,prediction,label):
+        assert (label.shape == prediction.shape)
+        # transpose the matrices
+        label = np.transpose(label)
+        prediction = np.transpose(prediction)
+        # the function derivative
+        self.loss_derivative = -np.transpose(((label - prediction) / row_len))
+        # the function output
+        col_len, row_len = label.shape
+        return np.sum(((np.multiply((label - prediction), (label - prediction))) / (2 * row_len)))
+
+    #return the previously calculated derivative
+    def backward(self):
+        return self.loss_derivative
 
 
+class multinomial_loss(loss_functions):
+    # general attributes
+    loss_derivative = None
 
-def multinomial_loss(prediction ,label,grad=False):
-    assert (label.shape == prediction.shape)
+    # init functions that take the model
+    def __init__(self, model=None):
+        self.model = model
+        self.loss_derivative = None
 
-    # the function output
-    if grad == False:
-        return -np.sum(np.multiply(np.log(prediction+np.exp(-15)),label ))
-    # the function derivative
-    elif grad == True:
-        # return the derivative for each class by d itself
+    # calculate the function and the derivative but only return the function
+    def evaluate(self, prediction, label):
+        assert (label.shape == prediction.shape)
+        # the function derivative
+        # return the derivative for each class by d all other classes
         no_of_examples, no_of_classes = prediction.shape
         delta = np.zeros((no_of_examples, no_of_classes))
         for j in range(no_of_examples):
             for i in range(no_of_classes):
-                if  label[j][i]==1:
+                if label[j][i] == 1:
                     delta[j][i] = -(1 - prediction[j][i])
                 else:
                     delta[j][i] = prediction[j][i]
-        return delta
+        self.loss_derivative =  delta
 
+        # the function output
+        return -np.sum(np.multiply(np.log(prediction + np.exp(-15)), label))
+
+    # return the previously calculated derivative
+    def backward(self):
+        return self.loss_derivative
 
 
