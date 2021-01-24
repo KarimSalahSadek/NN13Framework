70efee5d245b56fda3ad23852ce6f00ff16c78a5
Karim Salah <karimsalahsadek@gmail.com>
2021-01-24 20:26:00 +0200

    Added new layers
    -Added CNN
    -Added Max_pool
    -Added Average_pool

--- nn13framework/neural_networks/__init__.py
+++ nn13framework/neural_networks/__init__.py
@@ -2,9 +2,14 @@ import numpy as np
 import nn13framework.neural_networks.layers as layers
 
 class model:
-
-    #History dictionary is made to save training data NOT model parameters
+    """
+        A neural network model object which holds all the paramters and the architicture of the neural network
+    """
     def __init__(self,name = None,date_created = None):
+        '''
+            name (optional): string that gives a name for the model
+            date_created (optional): string to save the date_created, the user provides the correct date themselves
+        '''
         self.name = name
         self.date_created = date_created
         self.weights = None #list of numpy array later
@@ -13,18 +18,36 @@ class model:
         self.history['true_negatives'] = []
         self.history['false_positives'] = []
         self.history['false_negatives'] = []
-        self.history['accuracy'] = []
-        self.history['validation_accuracy'] = []
+        self.history['accuracy'] = [0]
+        self.history['validation_accuracy'] = [0]
         self.history['batch_loss'] = []
-        self.history['epoch_loss'] = []
-        self.history['validation_loss'] = []
+        self.history['epoch_loss'] = [None]
+        self.history['validation_loss'] = [None]
         self.history['precision'] = []
         self.history['recall']= []
         self.history['f1score'] = []
-        self.layers = None #list of layers later
+        self.history['epoch_number'] = 0
+        self.layers = [] #list of layers later
         self.evaluate_mode = False
         pass
     
+    def reset_history(self):
+        '''
+            Resets the history directory for the model
+            The history directory is used to store information about the last training session
+        '''
+        self.history = {}
+        self.history['true_positives'] = []
+        self.history['true_negatives'] = []
+        self.history['false_positives'] = []
+        self.history['false_negatives'] = []
+        self.history['accuracy'] = [0]
+        self.history['validation_accuracy'] = [0]
+        self.history['batch_loss'] = []
+        self.history['epoch_loss'] = [None]
+        self.history['validation_loss'] = [None]
+        self.history['epoch_number'] = 0
+
     def set_evaluate_mode(self,mode):
         '''
             Sets the mode (boolean) of the model to either training mode or evaluation mode
@@ -36,23 +59,26 @@ class model:
             if layer.layer_name == 'dropout':
                 layer.evaluate_mode = mode
     
-    #Initializes self.weights , has severals modes
-    def initialize_weights(self,mode,a, b = None):
+    def initialize_weights(self,mode,a = None , b = None):
         '''
-        mode can take three values:
-        1- 'const': in this case all weights are initialized to the constant value a, while b is discarded
-        2- 'normal_dist' in this case all weights are initialized from a normal distribution
-        with mean = a and std_deviation = b
-        3- 'uniform_dist' in this case all weights are initialized from a uniform distribution
-        with min = a and max = b
+            Initializes self.weights , has severals modes
+            mode can take four values:
+            1- 'const': in this case all weights are initialized to the constant value a, while b is discarded
+            2- 'normal_dist' in this case all weights are initialized from a normal distribution
+            with mean = a and std_deviation = b
+            3- 'uniform_dist' in this case all weights are initialized from a uniform distribution
+            with min = a and max = b
+            4- 'xavier'  in this case all weights are initialized using the Xavier Initialization method
         '''
         if(mode == 'const'):
+            assert(a is not None),'a parameter (const) must be provided for this mode'
             for layer in self.layers:
                 if layer.weight is None:
                     continue
                 shape = tuple(layer.weight.shape)
                 layer.weight = np.ones(shape) * a
         elif (mode == 'normal_dist'):
+            assert(a is not None),'a parameter (std_deviation) must be provided for this mode'
             assert(b is not None),'b parameter (std_deviation) must be provided for this mode'
             for layer in self.layers:
                 if layer.weight is None:
@@ -60,12 +86,21 @@ class model:
                 shape = layer.weight.shape
                 layer.weight = np.random.normal(a,b,shape)
         elif (mode == 'uniform_dist'):
+            assert(a is not None),'a parameter (uniform_dist_max) must be provided for this mode'
             assert(b is not None),'b parameter (uniform_dist_max) must be provided for this mode'
             for layer in self.layers:
                 if layer.weight is None:
                     continue
                 shape = layer.weight.shape
                 layer.weight = np.random.uniform(a,b,shape)
+        elif (mode == 'xavier'):
+            for layer in self.layers:
+                if layer.weight is None:
+                    continue
+                shape = layer.weight.shape
+                if layer.layer_name == 'convolution':
+                    continue
+                layer.weight = np.random.randn(shape[0],shape[1])*np.sqrt(2/layer.input_dim)
 
 
         self.weights = [layer.weight for layer in self.layers if layer.weight is not None]
@@ -84,23 +119,30 @@ class model:
         '''
             Adds a layer to the model, layer is an 'nn13framework.neural_network.layers.layer' child object
         '''
-        assert(hasattr(layer,'weight')),"Cannot add object belonging to abstract class 'layer'"
+        assert(hasattr(layer,'layer_name')),"Cannot add object belonging to abstract class 'layer'"
         assert(isinstance(layer,type(layers.layer()))),"Input must be a layer object!"
         if self.layers is None:
             self.layers = []
         if layer.is_activation:
-            assert(len(self.layers)>0),"Cannot have the first layer as an activation layer"
-            layer.input_dim = self.layers[-1].output_dim
-            layer.output_dim = layer.input_dim
-        if len(self.layers)>0:
-            assert(layer.input_dim == self.layers[-1].output_dim)
-        if not layer.is_activation:
+                assert(len(self.layers)>0),"Cannot have the first layer as an activation layer"
+        if layer.layer_name == 'linear' or layer.layer_name == 'linear_svm_output':
+            first = True
+            for searching_layer in self.layers:
+                if searching_layer.layer_name=='linear' or searching_layer.layer_name=='linear_svm_output' :
+                    first = False
+                    break
+            layer.first_linear = first
             new_weight = None
             if layer.use_bias:
-                new_weight = np.random.uniform(0,1/layer.input_dim**0.5,(layer.output_dim,layer.input_dim+1)) # +1 for bias
+                new_weight = np.ones((layer.output_dim,layer.input_dim+1)) # +1 for bias
             else:
-                new_weight = np.random.uniform(0,1/layer.input_dim**0.5,(layer.output_dim,layer.input_dim))
+                new_weight = np.ones((layer.output_dim,layer.input_dim))
+            self.initialize_weights('xavier',layer.input_dim)
             layer.weight = new_weight
+        elif layer.layer_name == 'convolution':
+            new_weight = np.random.randn(layer.n_filter, layer.d_X, layer.h_filter, layer.w_filter) * np.sqrt(2/layer.n_filter)
+            layer.weight =  new_weight
+
         layer_num = len(self.layers)
         layer.layer_num = layer_num
         self.layers.append(layer)
@@ -117,3 +159,24 @@ class model:
             self.layers[i].layer_num -= 1
 
         self.weights = [layer.weight for layer in self.layers if layer.weight is not None]
+
+class LeNet_5(model):
+    '''
+        LeNet_5 architecture
+        Can be loaded directly as a model object
+    '''
+    def __init__(self,name = 'LeNet_5',date_created = None):
+        super().__init__(name,date_created)
+        self.add_layer(layers.convolution(1,6,5,1,2))
+        self.add_layer(layers.tanh())
+        self.add_layer(layers.average_pool(2,2))
+        self.add_layer(layers.convolution(6,16,5,1,0))
+        self.add_layer(layers.average_pool(2,2))
+        self.add_layer(layers.flatten())
+        self.add_layer(layers.linear(400,120))
+        self.add_layer(layers.tanh())
+        self.add_layer(layers.linear(120,84))
+        self.add_layer(layers.tanh())
+        self.add_layer(layers.linear(84,10))
+        self.add_layer(layers.softmax())
+        
